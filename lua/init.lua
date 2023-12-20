874b6bf9996333d8e485417b4903d65153f59c22
Mark Wissink <markcwissink@gmail.com>
2023-12-20 07:49:56 -0500

    Use lazy

--- lua/init.lua
+++ lua/init.lua
@@ -1,36 +1,37 @@
--- Bootstrapping packer
--- https://github.com/wbthomason/packer.nvim#bootstrapping
-local ensure_packer = function()
-   local fn = vim.fn
-   local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-   if fn.empty(fn.glob(install_path)) > 0 then
-      fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-      vim.cmd('packadd packer.nvim')
-      return true
-   end
-   return false
+-- bootstrap lazy
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
-local packer_bootstrap = ensure_packer()
+vim.g.mapleader = " "
 
-return require('packer').startup(
-   function(use)
-      use { 'wbthomason/packer.nvim' }
-      use { "ellisonleao/gruvbox.nvim" }
-      use { 'mbbill/undotree' }
-      use {
-	 'nvim-telescope/telescope.nvim',
-	 tag = 0.1.5,
-	 requires = {
-	    'nvim-lua/plenary.nvim',
-	 },
-      }
-
-      -- Automatically set up your configuration after cloning packer.nvim
-      local packer = require('packer')
-      if packer_bootstrap then
-	 packer.sync()
-      end
-      packer.install()
-      packer.compile()
-   end)
+require("lazy").setup({
+  { 
+    "savq/melange-nvim",
+    config = function()
+      vim.opt.termguicolors = true
+      vim.cmd.colorscheme 'melange'
+    end
+  },
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.5',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+    end
+  }
+})
