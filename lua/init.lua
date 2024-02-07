ec59ccc66dc704cfab9f61b028cc2cde38e83991
Mark Wissink <markcwissink@gmail.com>
2024-02-07 18:40:21 -0500

    update

--- lua/init.lua
+++ lua/init.lua
@@ -12,6 +12,7 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 vim.g.mapleader = " "
+vim.o.signcolumn = "yes"
 
 require("lazy").setup({
 	{
@@ -25,17 +26,26 @@ require("lazy").setup({
 	{
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.5",
-		dependencies = { "nvim-lua/plenary.nvim" },
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope-ui-select.nvim",
+			"debugloop/telescope-undo.nvim",
+		},
 		config = function()
 			require("telescope").setup({
-				defaults = require("telescope.themes").get_dropdown()
+				defaults = require("telescope.themes").get_dropdown(),
 			})
+			require("telescope").load_extension("ui-select")
+			require("telescope").load_extension("undo")
+
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<leader>f", builtin.find_files, {})
 			vim.keymap.set("n", "<leader>g", builtin.live_grep, {})
-			vim.keymap.set("n", "<leader>b", builtin.buffers, {})
+			vim.keymap.set("n", "<leader>b",
+				function() builtin.buffers({ sort_mru = true, ignore_current_buffer = true }) end, {})
 			vim.keymap.set("n", "<leader>s", builtin.current_buffer_fuzzy_find, {})
 			vim.keymap.set("n", "<leader>t", builtin.treesitter, {})
+			vim.keymap.set("n", "<leader>u", "<cmd>Telescope undo<cr>", {})
 		end
 	},
 	{
@@ -43,6 +53,15 @@ require("lazy").setup({
 		build = 'make',
 		config = function()
 			require('telescope').load_extension('fzf')
+		end
+	},
+	{
+		"nvim-telescope/telescope-file-browser.nvim",
+		dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+		config = function()
+			require("telescope").load_extension("file_browser")
+			vim.keymap.set("n", "<leader>d", "<cmd>Telescope file_browser path=%:p:h select_buffer=true<cr>",
+				{})
 		end
 	},
 	{
@@ -78,7 +97,9 @@ require("lazy").setup({
 		'hrsh7th/nvim-cmp',
 		event = 'InsertEnter',
 		dependencies = {
-			{ 'L3MON4D3/LuaSnip' },
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			"L3MON4D3/LuaSnip",
 		},
 		config = function()
 			local lsp_zero = require('lsp-zero')
@@ -95,7 +116,17 @@ require("lazy").setup({
 					['<C-d>'] = cmp.mapping.scroll_docs(4),
 					['<C-f>'] = cmp_action.luasnip_jump_forward(),
 					['<C-b>'] = cmp_action.luasnip_jump_backward(),
+					['<CR>'] = cmp.mapping.confirm({ select = true }),
 				})
+			})
+
+			cmp.setup.cmdline(":", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = cmp.config.sources({
+					{ name = "path" },
+				}, {
+					{ name = "cmdline" },
+				}),
 			})
 		end
 	},
@@ -109,13 +140,19 @@ require("lazy").setup({
 		},
 		config = function()
 			local lsp_zero = require('lsp-zero')
+			local builtin = require("telescope.builtin")
 			lsp_zero.extend_lspconfig()
 
 			lsp_zero.on_attach(function(client, bufnr)
 				-- see :help lsp-zero-keybindings
-				vim.keymap.set("n", "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<cr>", { buffer = bufnr })
+				vim.keymap.set("n", "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<cr>",
+					{ buffer = bufnr })
 				vim.keymap.set("n", "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<cr>", { buffer = bufnr })
-				lsp_zero.default_keymaps({ buffer = bufnr })
+				vim.keymap.set("n", "<leader>lf", "<cmd>lua vim.lsp.buf.format()<cr>", { buffer = bufnr })
+				vim.keymap.set("n", "gd", builtin.lsp_definitions, { buffer = bufnr })
+				vim.keymap.set("n", "gi", builtin.lsp_implementations, { buffer = bufnr })
+				vim.keymap.set("n", "gr", builtin.lsp_references, { buffer = bufnr })
+				lsp_zero.default_keymaps({ buffer = bufnr, preserve_mappings = true })
 			end)
 
 			require('mason-lspconfig').setup({
